81ccbadc0d5bb3bc45ce9be187e134d6c4dda8fd
KeYan-clone <keyanli259@gmail.com>
2024-11-12 20:11:15 +0800

    feats:add ServerEventListener_CODE_ROOM_CREATE

---
+++ src/main/java/buaa/oop/landlords/server/event/ServerEventListener_CODE_ROOM_CREATE.java
@@ -0,0 +1,10 @@
+package buaa.oop.landlords.server.event;
+
+import buaa.oop.landlords.common.entities.ClientEnd;
+
+public class ServerEventListener_CODE_ROOM_CREATE extends ServerEventListener{
+    @Override
+    public void call(ClientEnd event, String data) {
+
+    }
+}
