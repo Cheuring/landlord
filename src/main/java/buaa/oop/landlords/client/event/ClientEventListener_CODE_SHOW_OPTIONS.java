111d4a9412fedf29e8f106766d19faa51875f2f9
Cheuring <dick_strong@outlook.com>
2024-11-12 19:59:20 +0800

    feat: add comments for ClientEventListener_CODE_SHOW_OPTIONS

--- src/main/java/buaa/oop/landlords/client/event/ClientEventListener_CODE_SHOW_OPTIONS.java
+++ src/main/java/buaa/oop/landlords/client/event/ClientEventListener_CODE_SHOW_OPTIONS.java
@@ -5,6 +5,11 @@ import io.netty.channel.Channel;
 
 import java.util.Scanner;
 
+/**
+ * 参考client_pvp
+ * 下个状态CODE_ROOM_CREATE CODE_GET_ROOMS
+ * 以及CODE_ROOM_JOIN
+ */
 public class ClientEventListener_CODE_SHOW_OPTIONS extends ClientEventListener {
 
     @Override
