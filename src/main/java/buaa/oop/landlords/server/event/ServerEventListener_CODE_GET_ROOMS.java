f28c791787f92749c0e7fe6987a9c4ca1579ea04
poi <3609300677@qq.com>
2024-11-12 20:09:22 +0800

    test

--- src/main/java/buaa/oop/landlords/server/event/ServerEventListener_CODE_GET_ROOMS.java
+++ src/main/java/buaa/oop/landlords/server/event/ServerEventListener_CODE_GET_ROOMS.java
@@ -1,6 +1,13 @@
 package buaa.oop.landlords.server.event;
 
-public class ServerEventListener_CODE_GET_ROOMS
-{
+import buaa.oop.landlords.common.entities.ClientEnd;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class ServerEventListener_CODE_GET_ROOMS extends ServerEventListener {
+
+    @Override
+    public void call(ClientEnd event, String data) {
+
+    }
 }
