36e7a3a3fc78b0515112c1dfb3130b31ec31fbae
poi <3609300677@qq.com>
2024-11-17 13:29:35 +0800

    feat:add Poker class and sortPokers function

--- src/main/java/buaa/oop/landlords/common/entities/Poker.java
+++ src/main/java/buaa/oop/landlords/common/entities/Poker.java
@@ -1,4 +1,41 @@
 package buaa.oop.landlords.common.entities;
 
+import buaa.oop.landlords.common.enums.PokerLevel;
+import buaa.oop.landlords.common.enums.PokerType;
+
 public class Poker {
+    private PokerLevel level;
+
+    private PokerType type;
+
+    public Poker(PokerLevel level, PokerType type) {
+        this.level = level;
+        this.type = type;
+    }
+
+    public PokerLevel getLevel() {
+        return level;
+    }
+
+    public PokerType getType() {
+        return type;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Poker other = (Poker) obj;
+        if (level != other.level)
+            return false;
+        return type == other.type;
+    }
+
+    @Override
+    public String toString() {
+        return level.getLevel() + " ";
+    }
 }
--- src/main/java/buaa/oop/landlords/common/utils/PokerUtil.java
+++ src/main/java/buaa/oop/landlords/common/utils/PokerUtil.java
@@ -2,9 +2,11 @@ package buaa.oop.landlords.common.utils;
 
 import buaa.oop.landlords.common.entities.Poker;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class PokerUtil {
+    private static final Comparator<Poker> pokerComparator = (o1, o2) -> o1.getLevel().getLevel() - o2.getLevel().getLevel();
     /**
      * 发牌
      * @return 三个玩家的牌 3*17+3
@@ -18,6 +20,6 @@ public class PokerUtil {
      * @param pokers 牌
      */
     public static void sortPokers(List<Poker> pokers) {
-
+        pokers.sort(pokerComparator);
     }
 }
