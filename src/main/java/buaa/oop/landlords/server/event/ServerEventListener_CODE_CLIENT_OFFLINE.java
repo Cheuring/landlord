493688cddef7ea369153d8c89585d2ec8bfeb5d6
Cheuring <dick_strong@outlook.com>
2024-11-18 12:44:28 +0800

    fix: fixed frame length and CHAT_FAIL name

--- src/main/java/buaa/oop/landlords/server/event/ServerEventListener_CODE_CLIENT_OFFLINE.java
+++ src/main/java/buaa/oop/landlords/server/event/ServerEventListener_CODE_CLIENT_OFFLINE.java
@@ -2,6 +2,7 @@ package buaa.oop.landlords.server.event;
 
 import buaa.oop.landlords.common.entities.ClientEnd;
 import buaa.oop.landlords.common.entities.Room;
+import buaa.oop.landlords.common.enums.RoomStatus;
 import buaa.oop.landlords.common.utils.MapUtil;
 import buaa.oop.landlords.server.ServerContainer;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ServerEventListener_CODE_CLIENT_OFFLINE extends ServerEventListener{
     @Override
-    public void call(ClientEnd event, String data) {
-        Room room = ServerContainer.ROOM_MAP.get(event.getRoomId());
+    public void call(ClientEnd client, String data) {
+        Room room = ServerContainer.ROOM_MAP.get(client.getRoomId());
+        if(room.getStatus() == RoomStatus.STARTING){
+            // todo: 通知其他玩家 游戏结束
+        }
 
-        ServerContainer.CLIENT_END_MAP.remove(event.getId());
+        ServerContainer.CLIENT_END_MAP.remove(client.getId());
 
-        log.info("Client {} | {} offline", event.getId(), event.getNickName());
+        log.info("Client {} | {} offline", client.getId(), client.getNickName());
     }
 }
