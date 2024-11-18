baaa26a7952a58ce80ab95d9a3a02b66475dfc46
KeYan-clone <keyanli259@gmail.com>
2024-11-18 16:51:58 +0800

    chores:fix view

--- src/main/java/buaa/oop/landlords/client/event/ClientEventListener_CODE_GAME_POKER_PLAY.java
+++ src/main/java/buaa/oop/landlords/client/event/ClientEventListener_CODE_GAME_POKER_PLAY.java
@@ -56,11 +56,6 @@ public class ClientEventListener_CODE_GAME_POKER_PLAY extends ClientEventListene
                 else{ pushToServer(channel, ServerEventCode.CODE_GAME_POKER_PLAY_PASS); }
             }
             else if (userInput.equalsIgnoreCase("view")||userInput.equalsIgnoreCase("v")) {
-                /*
-                 todo : fix the bug
-                 用户输入 v 时，不能Map<String, Object>roominfo= MapUtil.parse(data);
-                 */
-
                 printInfo(roominfo, pokers);
                 call(channel, data);
                 return;
